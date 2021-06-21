03b8900a2516ee93b421528b6350b867546b15ec
TeddyKiryakova <teakiryakova@gmail.com>
2021-06-21 13:47:53 +0100

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-   hi
    
+   <footer> This project was created by Teodora Kiryakova and is {""}
+   <a href="https://github.com/TeddyKiryakova/react-weather-app-project" target="_blank">
+     open-sourced on GitHub
+   </a>
+    </footer>
     </div>
   );
 }

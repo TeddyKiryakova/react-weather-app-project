b7ac33ba7a4b69bdcab11214f88b998d13862dcc
TeddyKiryakova <teakiryakova@gmail.com>
2021-06-21 14:20:39 +0100

    Bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import React  from 'react';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
    
    <footer> This project was created by Teodora Kiryakova and is {""}
-   <a href="https://github.com/TeddyKiryakova/react-weather-app-project" target="_blank">
+   <a href="https://github.com/TeddyKiryakova/react-weather-app-project" target="_blank" rel="noreferrer">
      open-sourced on GitHub
    </a>
     </footer>
+    </div>
     </div>
   );
 }

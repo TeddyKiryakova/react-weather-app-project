c568a65926a6410c294fe860b224e4230195cd33
TeddyKiryakova <teakiryakova@gmail.com>
2021-06-23 13:22:51 +0100

    Basic CSS added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React  from 'react';
-import './App.css';
+import React  from "react";
+import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-   
+        
+   <Weather />
    <footer> This project was created by Teodora Kiryakova and is {""}
    <a href="https://github.com/TeddyKiryakova/react-weather-app-project" target="_blank" rel="noreferrer">
      open-sourced on GitHub
